2a23af3393f5255841756e27fe12c020d8973196
damian at beagle-addacs <damian@frey.co.nz>
2012-05-07 17:06:42 +0100

    also attempting to read from adc, wip

--- addacs-daemon/main.c
+++ addacs-daemon/main.c
@@ -16,13 +16,163 @@ int i2c_fd = -1;
 int mm_fd = -1;
 IPCTestStruct* sharedData = 0;
 int shouldStop = 0;
+// adc has slave address 0x64 = 0b0110110
+#define ADC_ADDRESS 0b0110100
 
+void interrupt()
+{
+	shouldStop = 1;
+}
+
+// i2c line handling
+int i2c_open()
+{
+	// open the i2c bus
+	char * filename = "/dev/i2c-2";
+	if ( (i2c_fd=open(filename, O_RDWR))<0 )
+	{	
+		fprintf(stderr, "error opening i2c-2: %d %s\n", errno, strerror(errno) );
+		return 1;
+	}
+	else
+		return 0;
+
+}
+
+void i2c_close()
+{
+	close( i2c_fd );
+}
+
+
+// adc
+int adc_slaveAssign()
+{
+	if ( ioctl(i2c_fd, I2C_SLAVE, ADC_ADDRESS )<0 )
+	{
+		perror("addacs_daemon: couldn't assign adc address");
+		return 1;
+	}
+	return 0;
+}
+
+int adc_setup()
+{
+	if ( 0 != adc_slaveAssign() )
+		return 2;
+
+	uint8_t buf[3];
+	buf[0] = 0b10000000; // setup byte
+
+	if( write( i2c_fd, buf, 1 ) != 1 )
+	{
+		perror("addacs-daemon: failed to write adc setup byte" );
+		return 1;
+	}
+	return 0;
+}
+
+
+// read adc, channel 0-3
+// returns -1 on channel select failure, 
+// returns -2 on read failure
+// returns -3 on failure to assign ADC as I2C slave
+int32_t adc_read( uint8_t channel )
+{
+	if ( adc_slaveAssign() != 0 )
+		return -3;
+	// according to max11612-7 datasheet, 
+	// cs[3-0] select the channel
+	// we are always differentional, and
+	// we always have first chan +, second - so cs0 = 0 
+	// + we are diffirential so SGL = 0
+	// now cs3, cs2, cs1 select the channel
+	// everything else is 0
+	uint8_t buf[2];
+	buf[0] = 0b00000000; 
+	buf[0] |= (channel << 2);
+	if( write( i2c_fd, buf, 1 ) != 1 )
+	{
+		fprintf(stderr,"addacs-daemon: failed to write adc chan %i selection byte: err %i %s", channel, errno, strerror(errno) );
+		return -1;
+	}
+	// now read
+	if ( read( i2c_fd, buf, 2 ) != 2 )
+	{
+		fprintf(stderr,"addacs-daemon: failed to read 2 bytes from adc chan %i: err %i %s", channel, errno, strerror(errno) );
+		return -2;
+	}
+	// clear first 4 bits, as they are sent high
+	uint16_t result = (buf[0] & (0x0f));
+	// shift 4 bits to MSB of result
+	result <<= 8;
+	// OR in the last 8 bits
+	result |= buf[1];
+	return result;
+
+}
+
+// nunchuck
+
+int nunchuck_slaveAssign()
+{
+	// nunchuck address
+	int addr=0x52; 
+	if ( ioctl(i2c_fd, I2C_SLAVE, addr) < 0 )
+	{
+		fprintf(stderr, "error assigning I2C slave address %x: %d %s", addr, errno, strerror(errno) );
+		return 1;
+	}
+	return 0;
+}
+
+
+
+int nunchuck_setup()
+{
+	if ( nunchuck_slaveAssign()!= 0 )
+		return 1;
+	
+	// handshake with nunchuck
+	uint8_t buf[2];
+	buf[0] = 0x40;
+	buf[1] = 0x00;
+	if( write( i2c_fd, buf, 2 ) != 2 )
+	{
+		fprintf(stderr, "write to i2c failed: err %i %s\n", errno, strerror(errno) );
+		return 2;
+	}
+
+	return 0;
+}
+
+int nunchuck_read( uint8_t *buf )
+{
+	if ( 0 != nunchuck_slaveAssign() )
+		return 1;
+
+	buf[0] = 0;
+	if ( 1 != write( i2c_fd, buf, 1 ) )
+	{
+		perror( "nunchuck_read: write failed" );
+		return 2;
+	}
+
+	if( 6 != read( i2c_fd, buf, 6 ) )
+	{
+		perror( "nunchuck read: read failed" );
+		return 3;	
+	}
+	return 0;
+}	
+
+// application
 void cleanup()
 {
 
 	// cleanup
 	if ( i2c_fd )
-		close( i2c_fd );
+		i2c_close();
 	if ( sharedData )
 		munmap( sharedData, sizeof(IPCTestStruct ));
 	if ( mm_fd )
@@ -37,10 +187,6 @@ void cleanup()
 	printf("cleaned up\n");
 }
 
-void interrupt()
-{
-	shouldStop = 1;
-}
 
 int main( int argc, char**argv )
 {
@@ -63,41 +209,10 @@ int main( int argc, char**argv )
 	printf("deamon running, shared memory address is %s\n", SHM_NAME );
 
 	
-	// open the i2c bus
-	char * filename = "/dev/i2c-2";
-	if ( (i2c_fd=open(filename, O_RDWR))<0 )
-	{	
-		fprintf(stderr, "error opening i2c-2: %d %s\n", errno, strerror(errno) );
+	if ( 0 != i2c_open() )
+	{
 		return 1;
 	}
-
-	// nunchuck address
-	int addr=0x52; 
-	if ( ioctl(i2c_fd, I2C_SLAVE, addr) < 0 )
-	{
-		fprintf(stderr, "error assigning I2C slave address %x: %d %s", addr, errno, strerror(errno) );
-		cleanup();
-		return 1;
-	}
-
-	
-	// handshake with nunchuck
-	char buf[256];
-	buf[0] = 0x40;
-	buf[1] = 0x00;
-	if( write( i2c_fd, buf, 2 ) != 2 )
-	{
-		fprintf(stderr, "write to i2c failed: err %i %s\n", errno, strerror(errno) );
-	}
-
-
-	int bytesRead =read( i2c_fd, buf, 6 ); 
-	printf( "requested %i, got %i, contents:\n", 6, bytesRead );
-	for ( int i=0; i<6; i++ )
-	{
-		printf("  %02x", buf[i] );
-	}
-	printf("\n");
 
 
 	// open shared memory with address '/ipc_test', readwrite, with a+rw permissions
@@ -105,7 +220,6 @@ int main( int argc, char**argv )
 	if ( mm_fd < 0 )
 	{
 		fprintf(stderr,"shm_open failed: err %i %s\n", errno, strerror(errno));
-		cleanup();
 		return 1;
 	}
 	
@@ -114,7 +228,6 @@ int main( int argc, char**argv )
 	if ( result < 0 )
 	{
 		fprintf(stderr,"ftruncate failed: err %i %s\n", errno, strerror(errno));
-		cleanup();
 		return 1;
 	}
 
@@ -123,12 +236,25 @@ int main( int argc, char**argv )
 	if ( sharedData == NULL )
 	{
 		fprintf(stderr,"mmap failed: err %i %s\n", errno, strerror(errno));
-		cleanup();
 		return 1;
 	}
 
+	// setup the nunchuck
+	if ( 0 != nunchuck_setup() )
+	{
+		return 1;
+	}
+
+	// setup the adc
+	if ( 0 != adc_setup() )
+	{
+		return 1;
+	}
+	
+
 	
 	int readCount = 0;
+	uint8_t buf[128];
 	
 	// main loop
 	while ( !shouldStop )
@@ -140,20 +266,21 @@ int main( int argc, char**argv )
 		}
 		if ( readCount > 0 )
 		{
-			buf[0] = 0;
-			write( i2c_fd, buf, 1 );
-			int bytesRead =read( i2c_fd, buf, 6 ); 
-			for ( int i=0; i<4; i++ )
+
+			nunchuck_read( buf );
+			for ( int i=0; i<3; i++ )
 				sharedData->inputs[i] = buf[i];
+
+//			uint32_t value = adc_read(0);
+//			if ( value >= 0 )
+//				sharedData->inputs[3] = value;
+
 			sharedData->readSequenceId++;
 			readCount--;
 		}
 
-
-
 		usleep( 300);
 	}
 	
-	cleanup();
 }
 
